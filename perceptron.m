28dee786d2412fd6a82e641e47b7226bd754a131
Melecio Ponte <pontezambrano@gmail.com>
2014-10-05 22:58:37 -0430

    *Done with AND & OR *Nice formatted output

--- perceptron.m
+++ perceptron.m
@@ -1,15 +1,29 @@
-% Initialize the weights
+% Initialize values
 threshold = 0;
 learning_rate = 0.1;
 
-% AND training set
 training_set = [1, 0, 0;
                 1, 0, 1;
                 1, 1, 0;
                 1, 1, 1];
 
-desired = [0, 0, 0, 1];
+% AND desired values
+desired_and = [0, 0, 0, 1];
 
-weights = learn_w(training_set, desired, threshold, learning_rate);
+% OR training set
+desired_or = [0, 1, 1, 1];
 
+
+printf('Learning AND\n')
+weights = learn_w(training_set, desired_and, threshold, learning_rate);
+
+printf('Final weights for AND\n')
 disp(weights)
+printf('\n\n')
+
+printf('Learning OR\n')
+weights = learn_w(training_set, desired_or, threshold, learning_rate);
+
+printf('Final weights for OR\n')
+disp(weights)
+printf('\n\n')
